e38c55474c137421f82b0b5798b84636c0549f65
jianwind <jianwind@yeah.net>
2018-08-11 18:44:10 +0800

    Vue框架完成，剩余与springboot交互未完成

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import 'es6-promise/auto'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+import ElementUI from 'element-ui'
+import './assets/theme/element-#1C6132/index.css'
+Vue.use(ElementUI)
+
+// Vue.prototype.$utils = Utils;
+// Vue.prototype.$api = Api;
+// Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+//开发模式开启mock.js
+if (process.env.NODE_ENV === 'development') {
+  //require('./mock')
+}
+
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+Vue.use(Router)
+
+import Main from '@/page/Main'
+import Dashboard from '@/page/Dashboard'
+import Member from '@/page/Member'
+
+let routes = [{
+  path:'/',
+  component:Main,
+  hidden:true,
+  children:[{
+    path: '/',
+    component: Dashboard,
+    name:'首页'
+  }]
+}]
+
+routes.push({
+  path: '/member',
+  name: '会员管理',
+  component: Main,
+  iconCls: 'fa fa-user-circle-o',
+  children: [{
+    path: '/member/data',
+    component: Member,
+    name: '会员信息管理'
+  }]
+})
+
+const router = new Router({
+  routes:routes
+})
+
+export default router
